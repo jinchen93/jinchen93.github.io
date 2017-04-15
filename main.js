e48270af52b34b6b0ac3848074e80b9e3d01c0ef
Jin Chen <jinchen93@gmail.com>
2017-04-15 15:59:16 -0700

    Add button styling and file request support

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Empty for now
+function renderNewPage () {
+  pageName = this.value;
+  html = document.querySelector('html');
+  filename = `http://jinchen93.github.io/${pageName}.html`;
+
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filename, true);
+  xmlhttp.send()
+  text = xmlhttp.responseText
+
+  console.log(text)
+  // html.innerHTML = `<object type="text/html" data="${pageName}.html" ></object>`
+}
+
+buttons = document.querySelector('button');
+buttons.addEventListener("click", renderNewPage);
